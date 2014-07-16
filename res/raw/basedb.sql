c8705befed83f9e4f674711f4b21d047e1151212
Cleiton Dantas dos Anjos <cleiton.dantas@outlook.com>
2014-07-16 08:43:28 -0300

    Update basedb.sql

--- res/raw/basedb.sql
+++ res/raw/basedb.sql
@@ -1,10 +1,6 @@
-INSERT or replace INTO TITULOS (TITULO,GRUPO,IDORACAO) VALUES ('Pai Nosso','Comum',0);
-INSERT or replace INTO TITULOS (TITULO,GRUPO,IDORACAO) VALUES ('Ave Maria','Comum',1);
+INSERT or replace INTO TITULOS (TITULO,GRUPO,IDORACAO) VALUES ('Pai Nosso','1',0);
+INSERT or replace INTO TITULOS (TITULO,GRUPO,IDORACAO) VALUES ('Ave Maria','1',1);
 INSERT or replace INTO TITULOS (TITULO,GRUPO,IDORACAO) VALUES ('Creio','Comum',2);
-INSERT or replace INTO TITULOS (TITULO,GRUPO,IDORACAO) VALUES ('Arcandjo','Intercessão',3);
-INSERT or replace INTO TITULOS (TITULO,GRUPO,IDORACAO) VALUES ('Serafim ','Intercessão',4);
-INSERT or replace INTO TITULOS (TITULO,GRUPO,IDORACAO) VALUES ('Querubim','Intercessão',5);
-INSERT or replace INTO TITULOS (TITULO,GRUPO,IDORACAO) VALUES ('Creio','Intercessão',6);
 
 INSERT or replace INTO ORACAO (TITULO,ORACAO,IDORACAO) VALUES ('Pai Nosso','Pai nosso que estás nos céus,<br/> santificado seja o Vosso nome. Venha a nós o Vosso Reino.<br/> Seja feita a Vossa vontade,<br/> assim na Terra como no Céu.<br/> O pão nosso de cada dia nos dai hoje.<br/> Perdoai as nossas ofensas assim como nós perdoamos a quem nos tem ofendido.<br/> E não nos deixeis cair em tentação,<br/> mas livrai-nos do mal.',0);
 INSERT or replace INTO ORACAO (TITULO,ORACAO,IDORACAO) VALUES ('Ave Maria','Ave Maria cheia de graça,<br/> o Senhor é convosco, bendita sois Vós entre as mulheres,<br> e bendito é o fruto do vosso ventre,<br/> Jesus. Santa Maria, Mãe de Deus,<br/> rogai por nós pecadores,<br/> agora e na hora da nossa morte.<br/> Amém',1);
