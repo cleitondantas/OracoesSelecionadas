10755ca217f8494ef5bfc048a1ed31739da63e82
Cleiton Dantas <cleiton.dantas@outlook.com>
2014-07-19 15:31:25 -0300

    Recriado O Main com NavegationDrawerFragment E incorpotado no projeto os layouts de menu Drawer

--- src/com/grupoeternaalianca/oracoesselecionadas/MainActivity.java
+++ src/com/grupoeternaalianca/oracoesselecionadas/MainActivity.java
@@ -1,9 +1,12 @@
 package com.grupoeternaalianca.oracoesselecionadas;
 
+import android.app.Activity;
 import android.content.*;
 import android.database.sqlite.*;
 import android.os.*;
-import android.support.v4.app.*;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.*;
 import android.view.*;
 import android.widget.*;
@@ -14,7 +17,18 @@ import com.grupoeternaalianca.oracoesselecionadas.vo.*;
 
 import java.util.*;
 
-public class MainActivity extends ActionBarActivity{
+ public class MainActivity extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks{
+	
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
+	
 	private PersistenceDao persistenceDao = new PersistenceDao();
 	public static SQLiteDatabase bancoDados = null;
 	private static final String DATABASE_NAME = "ORACOES_SELECIONADAS_DB";
@@ -27,15 +41,20 @@ public class MainActivity extends ActionBarActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null){
-            getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
-        }
+        getActionBar().setDisplayShowTitleEnabled(true);
+        mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);   
+        
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(  R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
+        
     	persistenceDao.openOrCreateDB(openDB());
     	if(! persistenceDao.verificaBancoExistente(openDB())){
     		persistenceDao.criaConteudo(openDB(),this);
     	}
+    	int i=1;
 		for (TituloVO titulos : persistenceDao.buscaTitulos(openDB())){
-			itens.add("Oração " + titulos.getTitulo());
+		
+			itens.add(	i++ +") " + titulos.getTitulo());
 		}
 		
         listView = (ListView)findViewById(R.id.listView1);
@@ -81,20 +100,7 @@ public class MainActivity extends ActionBarActivity{
         }
         return super.onOptionsItemSelected(item);
     }
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment{
 
-        public PlaceholderFragment(){
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
     
 	public SQLiteDatabase openDB(){
 		try{
@@ -103,4 +109,76 @@ public class MainActivity extends ActionBarActivity{
 		}
 		return bancoDados;
 	}
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction() .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))  .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+        }
+    }
+    
+    
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
+//-----------------------------------------------------Classe     PlaceholderFragment
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
 }
