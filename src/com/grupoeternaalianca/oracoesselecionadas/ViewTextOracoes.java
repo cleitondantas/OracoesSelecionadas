aacdb2f4971508bed985ba089d38e5d1ca62dd77
Hp <Hp@HP-dv7-4073>
2014-10-01 22:14:59 -0300

    Incluido Pacote que Realiza o Justify no TextView

--- src/com/grupoeternaalianca/oracoesselecionadas/ViewTextOracoes.java
+++ src/com/grupoeternaalianca/oracoesselecionadas/ViewTextOracoes.java
@@ -4,16 +4,22 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.ViewTreeObserver.OnPreDrawListener;
 import android.widget.TextView;
 
 import com.grupoeternaalianca.oracoesselecionadas.dao.PersistenceDao;
+import com.grupoeternaalianca.oracoesselecionadas.util.TextJustifyUtils;
+import com.grupoeternaalianca.oracoesselecionadas.util.TextViewEx;
 import com.grupoeternaalianca.oracoesselecionadas.vo.OracaoVO;
+
 import android.text.*;
 
 public class ViewTextOracoes extends ActionBarActivity{
 	private PersistenceDao persistenceDao = new PersistenceDao();
 	private TextView tvTextOracao=null;
 	private TextView tvTituloOracao=null;
+	private TextViewEx textViewExs=null;
+	
 	public static SQLiteDatabase bancoDados = null;
 	private static final String DATABASE_NAME = "ORACOES_SELECIONADAS_DB";
 	 @Override
@@ -22,13 +28,18 @@ public class ViewTextOracoes extends ActionBarActivity{
 		 setContentView(R.layout.vieworacoes);
 		  Intent intent = getIntent();
 		  Bundle extras = intent.getExtras();
-		OracaoVO oracao = new OracaoVO();
-		oracao = persistenceDao.buscaOracao(openDB(),String.valueOf(extras.getInt("idOracao")));
+		  OracaoVO oracao = new OracaoVO();
+		  oracao = persistenceDao.buscaOracao(openDB(),String.valueOf(extras.getInt("idOracao")));
 		  tvTituloOracao = (TextView) findViewById(R.id.tvTitulo);
-		  tvTextOracao = (TextView) findViewById(R.id.tvOracao);
-		  
+		
+		  textViewExs = (TextViewEx) findViewById(R.id.tvOracao);
 		  tvTituloOracao.setText(Html.fromHtml(oracao.getTitulo()));
-		  tvTextOracao.setText(Html.fromHtml(oracao.getTexto().replace(".", ".<br/>").replace("!", "!<br/>")));
+		  String textoOracao = oracao.getTexto().replace(".", ".<br/>").replace("!", "!<br/>");
+		  
+		  Spanned htmlTextFormt = Html.fromHtml(textoOracao);
+		  
+		  textViewExs.setText(htmlTextFormt,true);
+			
 	 }
 		public SQLiteDatabase openDB(){
 			try{
