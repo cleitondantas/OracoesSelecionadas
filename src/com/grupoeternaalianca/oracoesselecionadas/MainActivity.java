a4c921c60995c17c718b0023fa3f440ff8db519a
Hp <Hp@HP-dv7-4073>
2014-10-17 11:19:06 -0300

    Arrumado layout do Search

--- src/com/grupoeternaalianca/oracoesselecionadas/MainActivity.java
+++ src/com/grupoeternaalianca/oracoesselecionadas/MainActivity.java
@@ -3,7 +3,6 @@ package com.grupoeternaalianca.oracoesselecionadas;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.R.integer;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.SearchManager;
